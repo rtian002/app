20f9c146cb6046fe8554eaeee2dd630789ee88a6
rtian002 <rtian002@gmail.com>
2024-08-19 08:41:48 +0800

    2024-08-19 更新程序,回写端口和UUID

--- start.sh
+++ start.sh
@@ -1,62 +1,53 @@
-#!/bin/bash
-#--------------
+#!/usr/bin/bash
+#--------------20240819
 # 先决条件：登录控制面板设置
-#		1. 添加一个UDP端口
-#		2. 设置允许“运行自己的程序（Run your own applications）”
+#  1. 添加一个UDP端口
+#  2. 设置允许“运行自己的程序（Run your own applications）”
 #--------------
-# 定义UDP端口（留空则使用默认16000）
-myport="16101"
-# 定义UUID（留空则使用随机）
-UUID=""
+UDP_PORT="16100"	# 定义UDP端口
+UUID="8fd27963-5dbb-11ef-996c-6cb311233542"		# 定义UUID（留空则使用随机）
 #================
-UDP_PORT=${myport:-"16000"}
-UUID=${UUID:-$(uuidgen)}
+[ "$#" -gt 0 ] && UDP_PORT=$1
+[ "$#" -eq 2 ] && UUID=$2
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
-pkill -kill -u $USERNAME > /dev/null & sleep 1
-echo "注销当前用户程序"
 if [[ "$HOSTNAME" == "s1.ct8.pl" ]];then
 	WORKDIR="${HOME}/domains/${USERNAME}.ct8.pl/app"
-	Title="CT8-S1"
+	Server="CT8-S1"
 else
 	WORKDIR="${HOME}/domains/${USERNAME}.serv00.net/app"
-	Title=$(echo $HOSTNAME|awk -F'.' '{print $2"-"$1}'|sed 's/s/S/g')
+	Server=$(echo $HOSTNAME|awk -F'.' '{print $2"-"$1}'|sed 's/s/S/g')
 fi
 [ -d "$WORKDIR" ] || (mkdir -p "$WORKDIR" && chmod 755 "$WORKDIR")
-
+#====================================================================
+pkill -kill -u $USERNAME > /dev/null 2>&1
 cd $WORKDIR
 if [ ! -f $WORKDIR/config.json ];then
-	wget https://github.com/rtian002/app/releases/download/Singbox/app.tar.gz && tar -zxvf app.tar.gz -C ./ 
-	sleep 1
+  wget http://appstores.ct8.pl/app.tar.gz && tar -zxvf app.tar.gz -C ./ 
+  sleep 1
+  UUID=${UUID:-$(uuidgen)}
+  cp config.json config
+  [ -n $UDP_PORT ] && sed -i '' "46s/16000/${UDP_PORT}/" config
+  sed -i '' "49s/ec97f674-c578-4940-9234-0a1da46041b9/${UUID}/" config
+else
+  [ "$#" -gt 0 ] && sed -i '' "46s/[0-9]\{1,5\}/${UDP_PORT}/" config
 fi
-
-cp config.json config
-sed -i '' "46s/16000/${UDP_PORT}/" config
-sed -i '' "49s/ec97f674-c578-4940-9234-0a1da46041b9/${UUID}/" config
-sleep 1
-echo "已初始化程序配置。"
+#-----------------------
 APPID="app-$(date +%s)"
 mv app-* $APPID
 nohup ./"$APPID" run -c config >/dev/null 2>&1 &
-echo "程序已运行..."
-
-get_ip() {
-	ip=$(curl -s --max-time 2 ipv4.ip.sb)
-	if [ -z "$ip" ]; then
-		if [[ "$HOSTNAME" =~ s[0-9]\.serv00\.com ]]; then
-			ip=${HOSTNAME/s/web}
-		else
-			ip="$HOSTNAME"
-		fi
-	fi
-	echo $ip
-}
-HOST_IP=$(get_ip)
-link="hysteria2://${UUID}@${HOST_IP}:${UDP_PORT}/?sni=www.bing.com&alpn=h3&insecure=1#${Title}%23${USERNAME}"
+echo "${APPID}: 新程序已运行..."
+#=========================================
+UUID=$(awk -F'"' 'NR==49 {print $4}' config)
+HOST_IP=$(curl -s --max-time 2 ipv4.ip.sb)
+link="hysteria2://${UUID}@${HOST_IP}:${UDP_PORT}/?sni=www.bing.com&alpn=h3&insecure=1#${Server}%23${USERNAME}"
 echo $link > ../logs/app.log
-
 echo -e "\e[1;32m"
 cat ../logs/app.log 
 echo -e "\033[0m"
 
-
+appstatus=$(pgrep app-)
+if [ $appstatus ];then
+  sed -i '' "7s/\".*\"/\"${UDP_PORT}\"/" ~/$0
+  sed -i '' "8s/\".*\"/\"${UUID}\"/" ~/$0
+fi
